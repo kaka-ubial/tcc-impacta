36d0ca2aa3fad5fec6659ec6df76bebfe7d5d66e
kaka-ubial <karen.ubial@seazone.com.br>
2026-08-29 13:16:38 -0300

    fixing dependency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,8 @@ use Illuminate\Validation\Rules\Password;
 use App\Models\Instituicao;
 use App\Observers\InstituicaoObserver;
 use Prometheus\CollectorRegistry;
+use Prometheus\Storage\APC;
+use Prometheus\Storage\InMemory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,9 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(CollectorRegistry::class, fn () => new CollectorRegistry(
-            new \Prometheus\Storage\APC()
-        ));
+        $this->app->singleton(CollectorRegistry::class, function () {
+            $storage = extension_loaded('apcu') && apcu_enabled()
+            ? new APC()
+            : new InMemory();
+
+            return new CollectorRegistry($storage);
+        });
     }
 
     /**
