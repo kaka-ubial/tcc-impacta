b3c29170c3508b2b46a15a85a376ed5f02059021
kaka-ubial <karen.ubial@seazone.com.br>
2026-08-29 14:48:20 -0300

    fix

--- app/Observers/TransferenciaMetricsObserver.php
+++ app/Observers/TransferenciaMetricsObserver.php
@@ -6,12 +6,6 @@ use App\Models\Transferencia;
 use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
 use Prometheus\CollectorRegistry;
 
-/**
- * Conta as entradas de transferencias em cada status, para o Prometheus.
- *
- * Mesma razao do DoacaoMetricsObserver para o ShouldHandleEventsAfterCommit:
- * o TransferenciaService envolve varias transicoes em DB::transaction().
- */
 class TransferenciaMetricsObserver implements ShouldHandleEventsAfterCommit
 {
     public function __construct(private readonly CollectorRegistry $registry) {}
